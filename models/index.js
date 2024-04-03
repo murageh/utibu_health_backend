b9f82bb9e7e7ed51fd8282b0075f9c638f859698
murageh <muragehdeveloper@gmail.com>
2024-04-03 23:12:17 +0300

    consolidate db associations
    Signed-off-by: murageh <muragehdeveloper@gmail.com>

--- models/index.js
+++ models/index.js
@@ -4,4 +4,47 @@ const Prescription = require("./Prescription");
 const Order = require("./Order");
 const OrderHistory = require("./OrderHistory");
 
+Medication.hasMany(Prescription, {
+    foreignKey: 'medication_id'
+});
+Prescription.belongsTo(Medication, {
+    foreignKey: 'medication_id',
+    as: 'medication'
+});
+
+User.hasMany(Prescription, {
+    foreignKey: 'user_id',
+    as: 'prescriptions'
+});
+Prescription.belongsTo(User, {
+    foreignKey: 'user_id',
+    as: 'user'
+});
+
+User.hasMany(Order, {
+    foreignKey: 'user_id',
+    as: 'orders'
+});
+Order.belongsTo(User, {
+    foreignKey: 'user_id',
+    as: 'user'
+});
+
+Prescription.hasOne(Order, {
+    foreignKey: 'prescription_id',
+    as: 'order'
+});
+Order.belongsTo(Prescription, {
+    foreignKey: 'prescription_id',
+    as: 'prescription'
+});
+
+Order.hasOne(OrderHistory, {
+    foreignKey: 'order_id'
+});
+
+User.hasMany(OrderHistory, {
+    foreignKey: 'user_id'
+});
+
 module.exports = {User, Medication, Prescription, Order, OrderHistory};
--- server.js
+++ server.js
@@ -8,52 +8,6 @@ app.use(express.urlencoded({extended: true}));
 
 // sync db
 const sequelize = require('./config/database');
-const {User, Medication, Order, Prescription, OrderHistory} = require("./models");
-
-Medication.hasMany(Prescription, {
-    foreignKey: 'medication_id'
-});
-Prescription.belongsTo(Medication, {
-    foreignKey: 'medication_id',
-    as: 'medication'
-});
-
-User.hasMany(Prescription, {
-    foreignKey: 'user_id',
-    as: 'prescriptions'
-});
-Prescription.belongsTo(User, {
-    foreignKey: 'user_id',
-    as: 'user'
-});
-
-User.hasMany(Order, {
-    foreignKey: 'user_id',
-    as: 'orders'
-});
-Order.belongsTo(User, {
-    foreignKey: 'user_id',
-    as: 'user'
-});
-
-Prescription.hasOne(Order, {
-    foreignKey: 'prescription_id',
-    as: 'order'
-});
-Order.belongsTo(Prescription, {
-    foreignKey: 'prescription_id',
-    as: 'prescription'
-});
-
-Order.hasOne(OrderHistory, {
-    foreignKey: 'order_id'
-});
-
-User.hasMany(OrderHistory, {
-    foreignKey: 'user_id'
-});
-
-// sync
 sequelize.sync({alter: true})
     .then(() => {
         console.log('Database synced successfully.');
